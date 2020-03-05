9417316ec5dc04c20407ef07309aec18326d2fc4
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-03-04 18:06:30 -0800

    Adding project

---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({title,calories,image,ingredients}) =>{
+    return(
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
+            <p>Calories: {parseInt(calories)} Kcal</p>
+            <img src={image} alt = "" className={style.image}></img>
+            <ul>
+                {ingredients.map ( ingredient=>
+                    <li>{ingredient.text}</li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default Recipe;
