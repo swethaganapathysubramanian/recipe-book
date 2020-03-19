02579f66858b20fa0021d6fdacdd908dd65aa5ad
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-03-18 19:56:53 -0700

    Updates

---
+++ precache-manifest.8f0d2c3d3c82e02848d32edc557d3b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7141200ae1dac5edd9bcb186bf679e",
+    "url": "/recipebook/index.html"
+  },
+  {
+    "revision": "3512f6c0add4c2273687",
+    "url": "/recipebook/static/css/main.f7deddeb.chunk.css"
+  },
+  {
+    "revision": "184b3908b31427996bbc",
+    "url": "/recipebook/static/js/2.6be66d5c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipebook/static/js/2.6be66d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3512f6c0add4c2273687",
+    "url": "/recipebook/static/js/main.554e8e78.chunk.js"
+  },
+  {
+    "revision": "490fb99447e7f64de312",
+    "url": "/recipebook/static/js/runtime-main.f7e886a5.js"
+  }
+]);
